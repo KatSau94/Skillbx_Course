ee8abf402b4833ec9146b8e6a619ca15a4a94321
Katarina <katsau94@gmail.com>
2021-06-24 14:07:00 +0300

    End of configuration client

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+
+import { Header } from "./Header";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<Header />, document.getElementById("react_root"));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html"
+        })
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+      },
+      module: {
+        rules: [
+          { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+      }
+    
+};
